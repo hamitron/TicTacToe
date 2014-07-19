7ef2a0044cb83a1153f1ed727e3653f818056810
hamitron <hqkarl@gmail.com>
2014-07-19 10:34:39 -0500

    ng repeat works, nothing else does

--- js/app.js
+++ js/app.js
@@ -4,35 +4,44 @@ TicTac.controller('TicTacController', function ($scope) {
 
 	$scope.boardName = 'tic tac fucking toe';
 
+
 	
 	$scope.gameBoard = [
 		{	row: 1,
 			column: 1,
 			uniqueId: "detroit"
-			},
-		{	row: 1,
-			column: 2
 		},
 		{	row: 1,
-			column: 3
+			column: 2,
+			uniqueId: "whiskey"
+		},
+		{	row: 1,
+			column: 3,
+			uniqueId: "tango"
 		},
 		{	row: 2,
-			column: 1
+			column: 1,
+			uniqueId: "bravo"
 		},
 		{	row: 2,
-			column: 2
+			column: 2,
+			uniqueId: "kilo"
 		},
 		{	row: 2,
-			column: 3
+			column: 3,
+			uniqueId: "victor"
 		},
 		{	row: 3,
-			column: 1
+			column: 1,
+			uniqueId: "lima"
 		},
 		{	row: 3,
-			column: 2
+			column: 2,
+			uniqueId: "charlie"
 		},
 		{	row: 3,
-			column: 3
+			column: 3,
+			uniqueId:"xray"
 		}
 	]
 
