feb75424b9996f356294504a214a846d9553e272
hamitron <hqkarl@gmail.com>
2014-07-22 17:39:26 -0700

    first attempt at logic and added images of xes and os

--- js/app.js
+++ js/app.js
@@ -14,35 +14,51 @@ $scope.createBoard = function(size) {
     }
 
  $scope.boardArray = [
-  {status: "A"}, 
-  {status: "B"}, 
-  {status: "C"}, 
-  {status: "D"}, 
-  {status: "E"}, 
-  {status: "F"}, 
-  {status: "G"}, 
-  {status: "H"}, 
-  {status: "I"}
+  {status: "A", idNum:0 }, 
+  {status: "B", idNum:1 }, 
+  {status: "C", idNum:2 }, 
+  {status: "D", idNum:3 }, 
+  {status: "E", idNum:4 }, 
+  {status: "F", idNum:5 }, 
+  {status: "G", idNum:6 }, 
+  {status: "H", idNum:7 }, 
+  {status: "I", idNum:8 }
   ]  ;
 
 };
+$scope.player1 = [];
+$scope.player2 = [];
 
 $scope.playcounter = 0;
 
   $scope.playerPicks = function(thisCell) {
     if ($scope.playcounter % 2 == 0){
       thisCell.status = "X" ;
+      $scope.player1.push(thisCell.idNum);
+      $scope.winScenario(thisCell.idNum);
+
     } else {
       thisCell.status = "O";
+      $scope.player2.push(thisCell.idNum);
+      $scope.winScenario(thisCell.idNum);
     }
       $scope.playcounter += 1;
+      console.log(thisCell.idNum);
+    
   } ;
 
 
 
 
-$scope.winScenario = function() {
-    console.log($scope.player1 ? "player1" : "player2");
+$scope.winScenario = function(playerId) {
+    if(playerId == 5){
+      if($scope.playcounter %2 ==0){
+        console.log("X WINS")
+      }else {
+        console.log("O Wins")
+      }
+      console.log("WINNER");
+    }
   }
   
 
