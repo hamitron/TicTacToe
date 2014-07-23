d99f3790f0433e4f4ab42104115998773377fd84
hamitron <hqkarl@gmail.com>
2014-07-22 17:06:09 -0700

    revised board, removed looping to create board in js in order to remove  from html.  ng-class now changes colors depending on player turn.

--- js/app.js
+++ js/app.js
@@ -1,52 +1,51 @@
-angular.module('TicTacToeApp', [
+var TTAPP = angular.module('TicTacToeApp', [
   'TicTacToeApp.controllers',
 ]);
 
 angular.module('TicTacToeApp.controllers', [])
 .controller('gameCtrl', ['$scope', function($scope) {
-  $scope.createBoard = function(size, symbol) {
+  
+
+$scope.createBoard = function(size) {
     // create a tic tac toe board using the argument size for width and height
     // if size is not set, use a 3 by 3 board
-
-    if (size == null) {
+  if (size == null) {
       size = 3;
     }
-    if (symbol == null) {
-      symbol = ' ';
-    }
-    $scope.board = [];
-    for(var i = 0; i < size; ++i) {
-      var temp = [];
-      for(var j = 0; j < size; ++j) {
-        temp.push(symbol); // set field to passed in symbol
-      }
-      $scope.board.push(temp);
-    }
-    console.log($scope.board);
-    $scope.boardcreated = true;
-  };
 
-  $scope.playcounter = 0;
-  $scope.player1 = false;
-  $scope.player2 = false;
+ $scope.boardArray = [
+  {status: "A"}, 
+  {status: "B"}, 
+  {status: "C"}, 
+  {status: "D"}, 
+  {status: "E"}, 
+  {status: "F"}, 
+  {status: "G"}, 
+  {status: "H"}, 
+  {status: "I"}
+  ]  ;
 
-  $scope.takefield = function(x, y) {
-    var symbol = "X";
-    if ($scope.playcounter % 2 == 0 ) {
-    	symbol = "X"
-      $scope.player1 = true;
+};
 
+$scope.playcounter = 0;
 
+  $scope.playerPicks = function(thisCell) {
+    if ($scope.playcounter % 2 == 0){
+      thisCell.status = "X" ;
     } else {
-    	symbol = "O"
+      thisCell.status = "O";
     }
+      $scope.playcounter += 1;
+  } ;
 
-    $scope.board[x][y] = symbol;
-    
-    $scope.playcounter += 1;
+
+
+
+$scope.winScenario = function() {
+    console.log($scope.player1 ? "player1" : "player2");
   }
+  
 
-
-  window.onload = $scope.createBoard(3);
+window.onload = $scope.createBoard(3);
   
 }]);
